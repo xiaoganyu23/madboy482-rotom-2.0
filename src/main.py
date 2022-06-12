ea60d9e380ed4fdc1883578571deffb980af6273
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-06-12 08:13:34 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -22,6 +22,7 @@ texts = json.load(open('src/texts.json', 'r'))
 data = json.load(open('src/pkmn.json', 'r'))
 stats = json.load(open('src/stats.json', 'r'))
 jtype = json.load(open('src/type.json', 'r'))
+ntype = json.load(open('src/natures.json', 'r'))
 
 usage_dict = {'vgc': None}
 
@@ -547,35 +548,15 @@ def bot_added(app, message):
 # ==== Types List =====
 def pnats_buttons(user_id):
     keyboard = ([[
-        InlineKeyboardButton('Lonely',callback_data=f"type_Lonely_{user_id}"),
-        InlineKeyboardButton('Fighting',callback_data=f"type_fighting_{user_id}"),
-        InlineKeyboardButton('Flying',callback_data=f"type_flying_{user_id}")]])
-    keyboard += ([[
-        InlineKeyboardButton('Poison',callback_data=f"type_poison_{user_id}"),
-        InlineKeyboardButton('Ground',callback_data=f"type_ground_{user_id}"),
-        InlineKeyboardButton('Rock',callback_data=f"type_rock_{user_id}")]])
-    keyboard += ([[
-        InlineKeyboardButton('Bug',callback_data=f"type_bug_{user_id}"),
-        InlineKeyboardButton('Ghost',callback_data=f"type_ghost_{user_id}"),
-        InlineKeyboardButton('Steel',callback_data=f"type_steel_{user_id}")]])
-    keyboard += ([[
-        InlineKeyboardButton('Fire',callback_data=f"type_fire_{user_id}"),
-        InlineKeyboardButton('Water',callback_data=f"type_water_{user_id}"),
-        InlineKeyboardButton('Grass',callback_data=f"type_grass_{user_id}")]])
-    keyboard += ([[
-        InlineKeyboardButton('Electric',callback_data=f"type_electric_{user_id}"),
-        InlineKeyboardButton('Psychic',callback_data=f"type_psychic_{user_id}"),
-        InlineKeyboardButton('Ice',callback_data=f"type_ice_{user_id}")]])
-    keyboard += ([[
-        InlineKeyboardButton('Dragon',callback_data=f"type_dragon_{user_id}"),
-        InlineKeyboardButton('Fairy',callback_data=f"type_fairy_{user_id}"),
-        InlineKeyboardButton('Dark',callback_data=f"type_dark_{user_id}")]])
+        InlineKeyboardButton('Lonely',callback_data=f"natures_lonely_{user_id}"),
+        InlineKeyboardButton('Fighting',callback_data=f"natures_adam_{user_id}"),
+        InlineKeyboardButton('Flying',callback_data=f"natures_hardy_{user_id}")]])
     keyboard += ([[
         InlineKeyboardButton('Delete',callback_data=f"hexa_delete_{user_id}")]])
     return keyboard
     
 @app.on_message(Filters.command(['natures', 'natures@MadBoy_Rotomgram2_Bot']))
-def types(app, message): 
+def natures(app, message): 
     user_id = message.from_user.id
     app.send_message(
         chat_id=message.chat.id,
